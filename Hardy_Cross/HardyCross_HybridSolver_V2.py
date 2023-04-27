0df8c5dfec56c71b17656ac53a0b48f671330c7d
AlexanderPerez23 <84990602+AlexanderPerez23@users.noreply.github.com>
2023-04-27 00:20:21 -0400

    Added Hardy Cross Stuff

--- Hardy_Cross/HardyCross_HybridSolver_V2.py
+++ Hardy_Cross/HardyCross_HybridSolver_V2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from copy import deepcopy
 
 def find_shared_indexes(arr):
@@ -69,51 +70,160 @@ def solve_hybrid_pipes(Q_loops, K_loops, loop_idexes,tol=1e-7, max_iter=100):
         count += 1
     return Q_loops_old, dQ, residuals
 
-print("""This program can solve a hybrid piping system with n symetrical loops\n""")
-n = float(input("Enter a value for n: "))
-C = float(input("Enter a value for constant C: "))
-k1 = float(input("Enter a value for constant k1: "))
-n_loops = int(input("Enter the number of loops in the system: "))
-n_lines = int(input("Enter the number of lines per loop: "))
-file_name = str(input("Enter file name for pipe data. Ensure its in the same directory as this program: "))
-# n = 1.852
-# C = 100
-# k1 = 4.727
-# n_loops = 5
-# n_lines = 3
-#
-# file_name = "Piping_Network_Data.csv"
-L = np.loadtxt(file_name, delimiter=",", usecols=1, skiprows=1)
-D = np.loadtxt(file_name, delimiter=",", usecols=2, skiprows=1)
-Q = np.loadtxt(file_name, delimiter=",", usecols=0, skiprows=1)
-K = np.zeros_like(L)
-loop_idexes = [0 for i in range(n_loops)]
-for i in range(n_loops):
-    col = int(4+i)
-    a = np.loadtxt(file_name, delimiter=',', usecols=col, skiprows=1, max_rows=n_lines)
-    loop_idexes[i] = a
+print("""
+#######################################################################################################################
+Hybrid Piping Line Solver (Hardy Cross Method)\n
+By: Alexander Perez and Joshua Nenadich\n
+This program can solve a hybrid piping system with n symetrical loops. Each loop must have the same number of lines. The
+User provides the type of pipe, the units of the volumetric flow rate Q, the number of loops in the system and the
+number of lines per loop.
 
-loop_idexes = np.vstack(loop_idexes)
+The User will also provide a file name of a csv (comma separated value) file that contains the following:
+Column 1 --> Volumetric Flow Rates for each line
+Column 2 --> Lengths of the Pipes in feet or meters
+Column 3 --> Diameters of the pipes in feet or meters
 
-for i in range(len(K)):
-    Length = L[i]
-    Diameter = D[i]
-    a = (k1 * Length) / (C ** n * Diameter ** (4.8704))
-    K[i] = float(a)
+There should be additional columns i.e. Column 4, 5, 6... that contain the lines that make up each loop
+#######################################################################################################################
+""")
+run = True
 
-K_loops = np.zeros_like(loop_idexes)
-Q_loops = np.zeros_like(loop_idexes)
-for j in range(len(loop_idexes)):
-    for i in range(len(loop_idexes[0])):
-        a = loop_idexes[j][i]
-        print(a)
-        K_loops[j][i] = K[int(a)]
-        Q_loops[j][i] = Q[int(a)]
 
-Q_loops = Q_loops.tolist()
-K_loops =  K_loops.tolist()
-loop_idexes = loop_idexes.tolist()
-copy_loop_idexes = deepcopy(loop_idexes)
-Q_loops = flip_repeated_values(copy_loop_idexes,  Q_loops)
-Q_solution, dQ, Residuals = solve_hybrid_pipes(Q_loops, K_loops, loop_idexes)
-print(Q_solution)
+
+while run:
+    try:
+        n = 1.852
+        while True:
+            # n = float(input("Enter a value for n: "))
+            try:
+                C = float(input("Enter a value for constant C for the type of Pipe: "))
+
+            except ValueError:
+                print("Please enter a valid decimal or integer value for C")
+                # better try again... Return to the start of the loop
+                continue
+
+            else:
+                break
+
+        while True:
+            try:
+                k1 = str(input(
+                    "Specify the units of the Volumetric Flow Rate Q, type cfs for ft^3/s, mgd for million gals/day or cms for m^3/s: "))
+
+                if k1 == "cfs":
+                    k1 = 4.727
+                elif k1 == "mgd":
+                    k1 = 10.63
+                elif k1 == "cms":
+                    k1 == 10.466
+                else:
+                    raise ValueError
+
+            except ValueError:
+                print("Please enter cfs, mgd or cms")
+
+            else:
+                break
+
+        while True:
+            try:
+                n_loops = int(input("Enter the number of loops in the system: "))
+
+            except ValueError:
+                print("Please enter a valid integer for number of loops")
+                # better try again... Return to the start of the loop
+                continue
+
+            else:
+                break
+
+        while True:
+            try:
+                n_lines = int(input("Enter the number of lines per loop: "))
+
+            except ValueError:
+                print("Please enter a valid integer for number of lines")
+                # better try again... Return to the start of the loop
+                continue
+
+            else:
+                break
+
+        while True:
+            try:
+                file_name = str(input("Enter file name for pipe data. Ensure its in the same directory as this program: "))
+                file_exists = os.path.exists(file_name)
+                if not file_exists:
+                    raise FileNotFoundError
+                # print(file_exists)
+
+            except FileNotFoundError:
+                print("Please enter a valid file name and ensure it is in the same directory as this program")
+                # better try again... Return to the start of the loop
+                continue
+
+            else:
+                break
+
+        # C = 100
+        # k1 = 4.727
+        # n_loops = 5
+        # n_lines = 3
+        #
+        # file_name = "Piping_Network_Data.csv"
+        L = np.loadtxt(file_name, delimiter=",", usecols=1, skiprows=1)
+        D = np.loadtxt(file_name, delimiter=",", usecols=2, skiprows=1)
+        Q = np.loadtxt(file_name, delimiter=",", usecols=0, skiprows=1)
+        K = np.zeros_like(L)
+        loop_idexes = [0 for i in range(n_loops)]
+        for i in range(n_loops):
+            col = int(4 + i)
+            a = np.loadtxt(file_name, delimiter=',', usecols=col, skiprows=1, max_rows=n_lines)
+            loop_idexes[i] = a
+
+        loop_idexes = np.vstack(loop_idexes)
+
+        for i in range(len(K)):
+            Length = L[i]
+            Diameter = D[i]
+            a = (k1 * Length) / (C ** n * Diameter ** (4.8704))
+            K[i] = float(a)
+
+        K_loops = np.zeros_like(loop_idexes)
+        Q_loops = np.zeros_like(loop_idexes)
+        for j in range(len(loop_idexes)):
+            for i in range(len(loop_idexes[0])):
+                a = loop_idexes[j][i]
+                K_loops[j][i] = K[int(a)]
+                Q_loops[j][i] = Q[int(a)]
+
+        Q_loops = Q_loops.tolist()
+        K_loops = K_loops.tolist()
+        loop_idexes = loop_idexes.tolist()
+        copy_loop_idexes = deepcopy(loop_idexes)
+        Q_loops = flip_repeated_values(copy_loop_idexes, Q_loops)
+        Q_solution, dQ, Residuals = solve_hybrid_pipes(Q_loops, K_loops, loop_idexes)
+
+        count = 0
+        print("""
+    ########################################################################################################################
+    Volumetric Flowrates:
+        """)
+        for i in range(len(loop_idexes)):
+
+            for j in range(len(loop_idexes[0])):
+                print(f"Q{int(loop_idexes[i][j] + 1)} = {round(Q_solution[i][j], 3)}")
+
+        print("""
+        #######################################################################################################################
+        """)
+        a = input("Run again? (y/n): ")
+        if a == "y":
+            continue
+        else:
+            run = False
+
+    except:
+        print("It seems an error ocurred, please verify that the number of loops and lines entered mathces the "
+              "data in the csv file and that the csv file is made as detailed in the instructions.")
